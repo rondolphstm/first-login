d9ad640f6b22052f46cddf0ebd774244a3431937
rondolphstm <rondolphtst.m@gmail.com>
2022-08-11 13:43:39 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { useState } from 'react';
-import Login from './components/Login';
-import SecretStuff from './components/secretStuff'
-import './App.css';
+import { useState } from "react"
+import Login from "./components/Login"
+import SecretStuff from "./components/secretStuff"
+import "./App.css"
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
     <>
-    <header>
-      <h1>My First Login</h1>
-    </header>
-    {isLoggedIn
-    
-    ?<SecretStuff/>
-    :<Login setIsLoggedIn={setIsLoggedIn}/>
-  }
+      <header>
+        <h1>My First Login</h1>
+      </header>
+      {isLoggedIn ? <SecretStuff /> : <Login setIsLoggedIn={setIsLoggedIn} />}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
